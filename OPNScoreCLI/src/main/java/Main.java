b27f0c9a353a720a93506d8e50cfc48e8b5a61f7
Alex <alexa@nicolor.tech>
2020-12-20 01:27:15 -0500

    Make me feel like I have done something more than reading a bumch of documentation

---
+++ OPNScoreCLI/src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    String senarioPath = "";
+    if (!args[0].equals(""))
+      senarioPath = "scenario.json";
+    readSenario(senarioPath);
+  }
+
+  private static void readSenario(String path) {
+
+  }
+}
