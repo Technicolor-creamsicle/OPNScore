b360169c33fad5460a3a0ddea5cf02a6e1b6b8f5
Michael Matthews <michaeldylanmatthews@gmail.com>
2020-12-20 19:37:20 -0500

    Fixed Issue where too much info was being returned.

--- OPNScoreCLI/src/main/java/Check-For-Acc.ps1
+++ OPNScoreCLI/src/main/java/Check-For-Acc.ps1
@@ -1,18 +1,35 @@
-function Get-Acc-Existence {
+<#
+.SYNOPSIS
+ Verify if an account with given string exists.
+
+.DESCRIPTION
+See synopsis.
+
+.PARAMETER Username
+The string username that we're looking for.
+
+.EXAMPLE
+Get-AccExistence -Username "JarJar"
+
+.NOTES
+light input scrubbing not meant for direct user interaction.
+#>
+function Get-AccExistence {
     param (
         $Username
     )
     
     #makes parameter required in a non annoying way
-    if(-not($Username)) {Throw "Usage: Get-Acc-Existence -Username 'string'"}
+    if(-not($Username)) {Throw "Usage: Get-AccExistence -Username 'string'"}
 
     $Exists = $true
 
-    try { Get-LocalUser $Username -erroraction Stop}
+    # try to find the requested user, if it doesn't exist then the error it caught and variable set.
+    try { $Trash = Get-LocalUser $Username -erroraction Stop} # assign the result so it's not returned, and set the error action so it works properly with try / catch.
     catch { $Exists = $false }
 
     return $Exists
 }
 
 
-Get-Acc-Existence -Username "Michael"
+Get-AccExistence -Username "Michael"
