0993b446a67692c575ae7b2677d8407d7cd0963c
Alex <alexa@nicolor.tech>
2020-12-21 23:49:33 -0500

    Syncing files between workstations.

--- OPNScoreCLI/src/main/java/Main.java
+++ OPNScoreCLI/src/main/java/Main.java
@@ -1,14 +1,28 @@
-
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    String senarioPath = "";
+    String scenarioPath = "";
     if (!args[0].equals(""))
-      senarioPath = "scenario.json";
-    readSenario(senarioPath);
+      scenarioPath = "scenario.json";
+    String rawScenario = readScenario(scenarioPath);
+    System.out.println(rawScenario);
   }
 
-  private static void readSenario(String path) {
+  private static String readScenario(String path) {
+    StringBuilder rawScenario = new StringBuilder();
+    try {
+      Scanner input = new Scanner(new File(path));
+      while (input.hasNextLine()) {
+        String temp = input.nextLine();
+        rawScenario.append(temp);
 
+      }
+      input.close();
+    } catch (Exception e) {
+      System.out.println("Error reading or parsing " + path);
+    }
+    return rawScenario.toString();
   }
 }
